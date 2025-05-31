1b8c475be32606a755006596eb71e95de3a1bf76
Fabricio da Silva Ferreira <fabricioferreira123.ff@gmail.com>
2025-05-31 06:15:31 -0300

    correcao voltar descricao

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,7 +1,8 @@
 import { ChevronLeftIcon } from "lucide-react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
+  const navigate = useNavigate();
   const [searcParams] = useSearchParams();
   const title = searcParams.get("title");
   const description = searcParams.get("description");
@@ -10,7 +11,10 @@ function TaskPage() {
     <div className="h-screen w-screen  bg-slate-500 P-6 flex justify-center">
       <div className="w-[500px] p-4">
         <div className=" flex justify-center relative mb-6">
-          <button className="absolute left-0 top-0 bottom-0 text-slate-100">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
             <ChevronLeftIcon />
           </button>
           <h1 className="text-3xl text-slate-100 font-bold text-center">
